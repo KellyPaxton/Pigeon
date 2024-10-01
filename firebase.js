d0f42757921861b12e9c0e27d5e79d375d006e58
Kelly Paxton <klypxtn@gmail.com>
2024-10-01 02:07:26 -0600

    Changed database permissions

--- firebase.js
+++ firebase.js
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCCuAiD5UlqnULHWJWzkgRx9YEsX94oYFU",
   authDomain: "pigeon-0.firebaseapp.com",
@@ -17,7 +12,6 @@ const firebaseConfig = {
   measurementId: "G-HES1SMFEPE"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
